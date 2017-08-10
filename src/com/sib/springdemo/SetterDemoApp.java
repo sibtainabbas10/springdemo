bdfcc0b74997ee2cfc7618e3d1ede4936f1522c4
sibtainabbas <sibtain.abbas10@gmail.com>
2017-08-10 23:22:48 +0500

    dependency injection - setter injection

--- src/com/sib/springdemo/SetterDemoApp.java
+++ src/com/sib/springdemo/SetterDemoApp.java
@@ -1,24 +1,21 @@
 package com.sib.springdemo;
 
-import org.springframework.beans.BeansException;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Created by C_v on 8/2/2017.
+ * Created by C_v on 8/10/2017.
  */
-public class HelloSpringApp {
+public class SetterDemoApp {
     public static void main(String[] args) {
-        // Load the spring configuration file (Create a Spring Container)
+        // load the spring configuration file
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        // Retreive bean from spring container
-        ICoach theCoach = context.getBean("myCoach", ICoach.class);
+        // retreive bean from the spring container
+        CricketCoach theCoach = context.getBean("myCricketCoach", CricketCoach.class);
 
         // call methods on the bean
         System.out.println(theCoach.getDailyWorkout());
-
-        // let's call our new method for fortunes
         System.out.println(theCoach.getDailyFortune());
 
         // close the context
