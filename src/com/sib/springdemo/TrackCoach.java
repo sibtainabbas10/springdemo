487e031b20213591024cb130c6921f36bffac55a
sibtainabbas <sibtain.abbas10@gmail.com>
2017-08-10 07:48:45 +0500

    xml dependency injection - contructor complete

--- src/com/sib/springdemo/TrackCoach.java
+++ src/com/sib/springdemo/TrackCoach.java
@@ -1,9 +1,19 @@
 package com.sib.springdemo;
 
+import javax.sound.midi.Track;
+
 /**
  * Created by C_v on 8/2/2017.
  */
 public class TrackCoach implements ICoach {
+    private IFortuneService fortuneService;
+
+    public TrackCoach() {}
+
+    public TrackCoach(IFortuneService fortuneService) {
+        this.fortuneService = fortuneService;
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Run a hard 5k";
@@ -11,6 +21,6 @@ public class TrackCoach implements ICoach {
 
     @Override
     public String getDailyFortune() {
-        return null;
+        return "Just do it! - " + fortuneService.getFortune();
     }
 }
